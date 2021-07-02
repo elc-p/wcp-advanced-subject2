bf04ec4542dcc049fed27aeece28452a4d201ea4
EC2 Default User <ec2-user@ip-172-31-74-205.ec2.internal>
2021-07-02 00:57:47 +0000

    finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:pages])
+    @book = Book.new
+    @sub_name = "Create Book"
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+    @sub_name = "Create Book"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
